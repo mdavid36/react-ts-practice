a8ab63a4393c6939424e6a067d0eb0d1fdeb50fb
dmatthew <dmatthew@cars.com>
2020-04-02 16:27:48 -0600

    create pokemon search

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,13 +1,75 @@
 import React, { Component } from 'react'
 import User from '../models/User.interface';
 
-export class PokemonSearch extends Component<User> {
+interface SearchState {
+  error: boolean,
+  pokemon: Pokemon
+}
+
+interface Pokemon {
+  name: string,
+  numberOfAbilities: number,
+  baseExperience: number, 
+  imageUrl: string
+}
+export class PokemonSearch extends Component<User, SearchState> {
+  pokemonRef: React.RefObject<HTMLInputElement>;
+  constructor(props: User){
+    super(props);
+    this.state = {
+      error: false,
+      pokemon: null
+    }
+    this.pokemonRef = React.createRef();
+  }
+
+  Search = () :void => {
+    const inputValue = this.pokemonRef.current.value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
+      .then(res => {
+        if (res.status !== 200) {
+          this.setState({error: true});
+          return
+        }
+        res.json().then(data => {
+          this.setState({
+            error: false,
+            pokemon: {
+              name: data.name,
+              numberOfAbilities: data.abilities.length,
+              baseExperience: data.baseExperience,
+              imageUrl: data.sprites.front_default
+            }
+          })
+        })
+      })
+  }
+
   render() {
-    const { name, numberOfPokemon } = this.props
-    console.log('>>>>> this.props', this.props);
+    const { name: userName, numberOfPokemon } = this.props;
+    const {
+      error, 
+      pokemon } = this.state;
+
+    let resultMarkup;
+
+    if(error) {
+      resultMarkup = <p>Pokemon not found</p>
+    } else if(this.state.pokemon){
+      resultMarkup = <div>
+        <img src={pokemon.imageUrl} alt='pokemon' className="pokemon-image"/>
+        <p>
+          {pokemon.name} has {pokemon.numberOfAbilities} number of abilities and a base experience of {pokemon.baseExperience}
+        </p>
+      </div>
+    }
+
     return (
       <div>
-        {name} {numberOfPokemon && <span>has {numberOfPokemon} pokemon</span>}
+        <p>{userName} {numberOfPokemon && <span>has {numberOfPokemon} pokemon</span>}</p>
+        <input type="text" ref={this.pokemonRef}/>
+        <button onClick={this.Search} className="search-button">Search</button>
+        {resultMarkup}
       </div>
     )
   }
